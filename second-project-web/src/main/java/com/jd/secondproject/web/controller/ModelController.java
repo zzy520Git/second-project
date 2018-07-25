ecfdda1ee6b04881c3233c05ab2ad3307ea1a68a
zhouzhongyi1 <zhouzhongyi1@jd.com>
2018-07-25 19:48:10 +0800

    velocity模板配置

--- second-project-web/src/main/java/com/jd/secondproject/web/controller/ModelController.java
+++ second-project-web/src/main/java/com/jd/secondproject/web/controller/ModelController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ModelController {
     @RequestMapping("/toIndex")
     public String toIndex() {
-        int a = 1 ;
         return "index" ;
     }
 }
