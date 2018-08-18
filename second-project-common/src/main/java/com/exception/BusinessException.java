5bd8e63cc541191ce90038e03cc1cee28239ea92
zhouzhongyi <zzy520com@qq.com>
2018-08-18 19:50:05 +0800

    RPC异常处理

--- second-project-common/src/main/java/com/exception/BusinessException.java
+++ second-project-common/src/main/java/com/exception/BusinessException.java
@@ -13,11 +13,25 @@ public class BusinessException extends RuntimeException {
         super("业务逻辑异常");
     }
 
+    public BusinessException(String msg) {
+        super(msg);
+    }
+
     public BusinessException(int code, String desc) {
         super(String.format("业务逻辑异常；Code:[%d], Description:[%s].", code, desc));
     }
 
     public static BusinessException asBusinessException() {
         return new BusinessException(ResponseResult.FAILURE, ResponseResult.FAILURE_DESC) ;
+    }
+
+    public static BusinessException rpcBusinessException() {
+        return new BusinessException("调用远程服务异常，请稍后再试！") ;
+    }
+    public static BusinessException clientBusinessException() {
+        return new BusinessException("服务异常，请稍后再试！") ;
+    }
+    public static BusinessException serviceBusinessException() {
+        return new BusinessException("系统内部异常，请稍后再试！") ;
     }
 }
